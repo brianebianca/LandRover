467819c9a8782299b4f6b3eeae8205ce28ffd1c7
Jessica Abreu <jessica@armazem.co>
2023-02-23 15:13:22 -0300

    create Core Layer and File Reader class

---
+++ LandRover.Core/Utils/FileReader.cs
@@ -0,0 +1,12 @@
+﻿namespace LandRover.Core.Utils
+{
+    public static class FileReader
+    {
+        public static string[] ReadFile(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+
+            return lines;
+        }
+    }
+}
--- LandRover.UI/Program.cs
+++ LandRover.UI/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using LandRover.Core.Utils;
+
+Console.WriteLine($"Hi");
+//string name = Environment.GetCommandLineArgs()[0];
+string file = Console.ReadLine();
+Console.WriteLine($"Reading file {file}!");
+
+var commands = FileReader.ReadFile( file );
+foreach (string line in commands)
+    Console.WriteLine(line);
